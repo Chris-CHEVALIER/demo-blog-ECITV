836f4eb2a6f5e28cf232480c843ab890289fd709
Chris <chevalier@chris-freelance.com>
2024-07-07 23:44:45 +0200

    fix html structure

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
-<div class="d-flex flex-wrap gap-5 justify-content-center">
+<?php
 
-    <?php
+require("./header.php");
+require("./connect.php");
 
-    require("./header.php");
-    require("./connect.php");
+echo '<div class="d-flex flex-wrap gap-5 justify-content-center">';
 
-    // Requête SQL pour récupérer tous les articles de le table
-    $result = $db->query("SELECT * FROM article");
+// Requête SQL pour récupérer tous les articles de le table
+$result = $db->query("SELECT * FROM article");
 
-    // S'il y a plus de 0 article
-    if ($result->num_rows > 0) {
-        // Parcours et affiche les titre et contenu chacun des articles enregistrés en BDD
-        while ($article = $result->fetch_assoc()) { ?>
-            <div class="card" style="width: 18rem;">
-                <img src="<?= $article["image_url"] ?>" class="card-img-top" alt="<?= $article["title"] ?>">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $article["title"] ?></h5>
-                    <p class="card-text"><?= $article["content"] ?></p>
-                    <a href="#" class="btn btn-primary">Voir plus</a>
-                </div>
+// S'il y a plus de 0 article
+if ($result->num_rows > 0) {
+    // Parcours et affiche les titre et contenu chacun des articles enregistrés en BDD
+    while ($article = $result->fetch_assoc()) { ?>
+        <div class="card" style="width: 18rem;">
+            <img src="<?= $article["image_url"] ?>" class="card-img-top" alt="<?= $article["title"] ?>">
+            <div class="card-body">
+                <h5 class="card-title"><?= $article["title"] ?></h5>
+                <p class="card-text"><?= $article["content"] ?></p>
+                <a href="#" class="btn btn-primary">Voir plus</a>
             </div>
-    <?php }
-    } else {
-        // Si aucun article n'existe, affiche ce message :
-        echo "Aucun article trouvé...";
-    }
+        </div>
+<?php }
+} else {
+    // Si aucun article n'existe, affiche ce message :
+    echo "Aucun article trouvé...";
+}
 
-    /* $article1 = [
+/* $article1 = [
         "title" => "Mon premier article",
         "content" => "Trop mignon !!! 😍",
         "image_url" => "https://meilleurseleveurs.com/wp-content/uploads/2023/11/Adoption-golden-retriever-2.jpg"
@@ -40,7 +40,7 @@
 
     $articles = [$article1, $article2];
  */
-    ?>
+?>
 
 </div>
 
